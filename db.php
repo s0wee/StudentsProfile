84dd81d0e0786f0b0a54d4da5a4fad7c253db458
kingpots <mart.edrian.bernardo3.com>
2023-11-17 08:22:30 +0800

    Acosta City add done

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 class Database {
     private $host = 'localhost';
     private $username = 'root';
-    private $password = 'root';
+    private $password = 'root1';
     private $database = 'school_db';
     private $conn;
 
--- town_city.php
+++ town_city.php
@@ -34,5 +34,88 @@ class TownCity {
             throw $e; // Re-throw the exception for higher-level handling
         }
     }
+
+    public function read($id) {
+        try {
+            $connection = $this->db->getConnection();
+
+            $sql = "SELECT * FROM students WHERE id = :id";
+            $stmt = $connection->prepare($sql);
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+
+            // Fetch the student data as an associative array
+            $studentData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            return $studentData;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            throw $e; // Re-throw the exception for higher-level handling
+        }
+    }
+
+    public function update($id, $data) {
+        try {
+            $sql = "UPDATE students SET
+                    student_number = :student_number,
+                    first_name = :first_name,
+                    middle_name = :middle_name,
+                    last_name = :last_name,
+                    gender = :gender,
+                    birthday = :birthday
+                    WHERE id = :id";
+
+            $stmt = $this->db->getConnection()->prepare($sql);
+            // Bind parameters
+            $stmt->bindValue(':id', $data['id']);
+            $stmt->bindValue(':student_number', $data['student_number']);
+            $stmt->bindValue(':first_name', $data['first_name']);
+            $stmt->bindValue(':middle_name', $data['middle_name']);
+            $stmt->bindValue(':last_name', $data['last_name']);
+            $stmt->bindValue(':gender', $data['gender']);
+            $stmt->bindValue(':birthday', $data['birthday']);
+
+            // Execute the query
+            $stmt->execute();
+
+            return $stmt->rowCount() > 0;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            throw $e; // Re-throw the exception for higher-level handling
+        }
+    }
+
+    public function delete($id) {
+        try {
+            $sql = "DELETE FROM students WHERE id = :id";
+            $stmt = $this->db->getConnection()->prepare($sql);
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+
+            // Check if any rows were affected (record deleted)
+            if ($stmt->rowCount() > 0) {
+                return true; // Record deleted successfully
+            } else {
+                return false; // No records were deleted (student_id not found)
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            throw $e; // Re-throw the exception for higher-level handling
+        }
+    }
+
+    public function displayAll(){
+        try {
+            $sql = "SELECT * FROM students LIMIT 10"; // Modify the table name to match your database
+            $stmt = $this->db->getConnection()->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        } catch (PDOException $e) {
+            // Handle any potential errors here
+            echo "Error: " . $e->getMessage();
+            throw $e; // Re-throw the exception for higher-level handling
+        }
+    }
 }
 ?>
